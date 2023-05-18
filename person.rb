e08045398ee1f1de32c82b85355d728a151e8c25
Adholah96 <adholah96@gmail.com>
2023-05-18 07:52:37 +0300

    Fix linters

--- person.rb
+++ person.rb
@@ -1,20 +1,25 @@
 class Person
-    attr_reader :id
-    attr_accessor : :name, :age
+  attr_reader :id
+  attr_accessor :name, :age
 
-    def initialize(age, name:"unkown", parent_permission:true)
-        @id = Random.rand(1..10001)
-        @name = name
-        @age = age 
-        @parent_permission = parent_permission
-    end
-    private
-    def of_age?
-        return true if @age >= 18
-        false
-    end
-    public
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  def initialize(age, name: 'unkown', parent_permission: true)
+    @id = Random.rand(1..1001)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    return true if @age >= 18
+
+    false
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
 class Student < Person
-  def initialize(age, classroom, name: "unknown", parent_permission: true)
+  def initialize(age, classroom, name: 'unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯(ツ)/¯'
   end
 end
