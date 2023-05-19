817143c977f0b065b1ed751257e3fe3b3b26fb0f
Adholah96 <adholah96@gmail.com>
2023-05-19 19:17:38 +0300

    Fix linters

--- student.rb
+++ student.rb
@@ -1,7 +1,6 @@
 require_relative './person'
 
 class Student < Person
-
   def initialize(age, classroom, name: 'unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
